8f7b55fd93d5f5d3467daf02e9a391d1cb76dd4f
jeseias <domingosjeseias@gmail.com>
2021-10-18 11:51:12 +0100

    refactor: group all protocols in a single file

---
+++ src/data/usecases/db-add-account/db-add-account-protocols.ts
@@ -0,0 +1,3 @@
+export { AccountModel } from '../../../domain/models/accounts'
+export { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+export { Encrypter } from '../../protocols/encrypter'
