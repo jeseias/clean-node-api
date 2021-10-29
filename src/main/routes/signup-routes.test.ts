44d62817e47c2fd9187c288324af26584c6bd080
jeseias <domingosjeseias@gmail.com>
2021-10-29 13:32:29 +0100

    test: ensure signup route returns an account on sucess

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Signup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({ name: 'Jeseias Dominogos', email: 'domingosjeseias@gmail.com', password: '123', passwordConfirmation: '123' })
+      .expect(200)
+  })
+})
